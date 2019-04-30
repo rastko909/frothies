b9ea64f81c13e0c6bc5dc7f464dd2817e3aedd78
rastko909 <rastko.beciric@gmail.com>
2019-05-01 09:35:40 +1000

    account seeds, including creating a vendor account works!

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = User.create(email: 'admin@gmail.com', password: 'password123', admin: true, vendor: false, first_name: 'Rastko', last_name: 'Beerboi')
+admin.save
+
+puts 'created admin'
+
+# we have to create a user before creating a vendor
+vendor_account = User.create(email: 'vendor@gmail.com', password: 'password123', admin: false, vendor: true, first_name: 'Beer', last_name: 'Master')
+
+# now we make a vendor, assign the vendor a user_id ! 
+vendor = Vendor.new(company_name: 'Big Boi Beers', sales_total: 0)
+vendor_account.id
+vendor.user_id = vendor_account.id
+vendor.save
+
+puts 'created vendor'
+
+customer = User.create(email: 'customer@gmail.com', password: 'password123', admin: false, vendor: false, first_name: 'Joe', last_name: 'Joe')
+customer.save
+
+puts 'created customer'

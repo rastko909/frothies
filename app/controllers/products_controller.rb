b17e14170551c14f0d9b780aa8e9c8dd9586718f
rastko909 <rastko.beciric@gmail.com>
2019-05-01 11:37:50 +1000

    multiple vendors with user association is working as intended.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,18 @@
 class ProductsController < ApplicationController
+
+  def index
+    if user_signed_in? 
+      @user = current_user
+      @user_id = current_user.id 
+      @first_name = current_user.first_name
+      @last_name = current_user.last_name 
+
+      if @user.vendor == true 
+        @user.vendors.each do |item|
+          @company_name = item.company_name
+        end
+      end
+    end
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,31 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-admin = User.create(email: 'admin@gmail.com', password: 'password123', admin: true, vendor: false, first_name: 'Rastko', last_name: 'Beerboi')
+admin = User.create(email: 'admin@gmail.com', password: 'password123', admin: true, vendor: false, first_name: 'Rastko', last_name: 'Beciric')
 admin.save
 
 puts 'created admin'
 
 # we have to create a user before creating a vendor
-vendor_account = User.create(email: 'vendor@gmail.com', password: 'password123', admin: false, vendor: true, first_name: 'Beer', last_name: 'Master')
-
-# now we make a vendor, assign the vendor a user_id ! 
-vendor = Vendor.new(company_name: 'Big Boi Beers', sales_total: 0)
-vendor_account.id
-vendor.user_id = vendor_account.id
+vendor_account = User.create(email: 'vendor@gmail.com', password: 'password123', admin: false, vendor: true, first_name: 'John', last_name: 'Doe')
+vendor = Vendor.new(company_name: 'Beer Master Beers', sales_total: 0, user_id: vendor_account.id)
 vendor.save
 
-puts 'created vendor'
+puts 'created vendor 1'
 
-customer = User.create(email: 'customer@gmail.com', password: 'password123', admin: false, vendor: false, first_name: 'Joe', last_name: 'Joe')
+vendor_account = User.create(email: 'furphy@gmail.com', password: 'password123', admin: false, vendor: true, first_name: 'Jane', last_name: 'Watson')
+vendor = Vendor.new(company_name: 'Furphy', sales_total: 0, user_id: vendor_account.id)
+vendor.save 
+
+puts 'created vendor 2'
+
+vendor_account = User.create(email: 'carlton@gmail.com', password: 'password123', admin: false, vendor: true, first_name: 'Bob', last_name: 'Bob')
+vendor = Vendor.new(company_name: 'Carlton', sales_total: 0, user_id: vendor_account.id)
+vendor.save 
+
+puts 'created vendor 3'
+
+customer = User.create(email: 'customer@gmail.com', password: 'password123', admin: false, vendor: false, first_name: 'Joe', last_name: 'Johnson')
 customer.save
 
 puts 'created customer'

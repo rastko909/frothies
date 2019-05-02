eeb28e7421f4d6ca5c217c5e802b17ba64fcd592
rastko909 <rastko.beciric@gmail.com>
2019-05-02 17:36:08 +1000

    fixed image storage

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,21 +7,25 @@ class ProductsController < ApplicationController
       @first_name = current_user.first_name
       @last_name = current_user.last_name 
 
-      if @user.vendor == true 
-        @user.vendors.each do |item|
-          @company_name = item.company_name
-        end
+      if @user.is_vendor? == true 
+          @company_name = @user.vendor.company_name
       end
     end
     @beers = Product.all 
   end
 
+  # def new 
+  #   @product = Product.new
+  # end
+
   def create
-    user = current_user.id.vendors.find(current_user.id).id 
-    raise 
     new_product = Product.new(product_params)
-    new_product.save
-    redirect_to products_path
+    new_product.vendor_id = current_user.vendor.id 
+    if new_product.save
+      redirect_to products_path
+    else
+      render 'new'
+    end
   end
 
   private

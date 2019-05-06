e9908e3f4627cb5f1183197dca2e2ea75d7d08d0
rastko909 <rastko.beciric@gmail.com>
2019-05-06 16:40:26 +1000

    if user signs up as vendor, created vendor account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  after_action :create_vendor_account, if: :devise_controller?
 
   protected
-
       def configure_permitted_parameters
           devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :first_name, :last_name, :address, :state, :postcode, :is_vendor])
+      end
+
+      def create_vendor_account
+        unless current_user == nil 
+            if current_user.is_vendor == true && current_user.vendor == nil 
+                vendor = Vendor.new(user_id: current_user.id)
+                vendor.save 
+            end
+        end
       end
 end

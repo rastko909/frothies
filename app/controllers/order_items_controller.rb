d77f89eb1dc274ffb6acca0b55c08a2a4afc2830
rastko909 <rastko.beciric@gmail.com>
2019-05-10 09:28:20 +1000

    fixed cart bug

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,7 +1,11 @@
 class OrderItemsController < ApplicationController
   def index 
+    if count_items_in_cart == 0 
+      flash[:alert] = "Cart is empty! Please add items before checking out."
+      redirect_to root_path
+    end
     @user = User.find(current_user.id)
-    @order_items = OrderItem.where(pending: true)
+    @order_items = OrderItem.where(pending: true, user_id: @user)
     @total_price = 0
     @order_items.each do |item|
       @total_price += item.product.price * item.quantity
@@ -32,8 +36,17 @@ class OrderItemsController < ApplicationController
     redirect_to order_items_path
   end
 
+  def count_items_in_cart
+    @cart_count = 0
+    unless current_user == nil
+    @cart_count = OrderItem.where(user: current_user.id, pending: true).count
+    end
+    return @cart_count
+  end
+
   private
   def order_items_params
     params.permit(:product_id, :quantity, :pending)
+
   end
 end

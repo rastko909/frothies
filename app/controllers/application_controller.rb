c423b8a3f128cb80485596f80ab7bd646b37fcab
jieyiphua <jieyiphua@gmail.com>
2019-05-09 14:22:08 +1000

    check what's wrong

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
   protected
       def configure_permitted_parameters
           devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :first_name, :last_name, :address, :state, :postcode, :is_vendor, :company_name, :company_abn, :company_description])
+          devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :first_name, :last_name, :address, :state, :postcode, :is_vendor, :company_name, :company_abn, :company_description])          
       end
 
       def create_vendor_account
